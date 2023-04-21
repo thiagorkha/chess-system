5d9c11b1c612d69b415d43b5ab2978a1d9f0c62d
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2023-04-21 18:22:38 +0000

    Criação classes Board e Piece

---
+++ boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+import boardgame.Position;
+Import boardgame.Board;
+
+public class Piece {
+
+  protected Position position;
+  private Board board;
+
+  
+
+    
+  
+
+  
+}
